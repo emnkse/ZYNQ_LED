10c6c20c979f2293a8c30591df5e8efca2cc7c46
emnkse <eminkose07@gmail.com>
2016-02-28 00:09:35 +0200

    LED Project

---
+++ Zynq_led/Zynq_led.sdk/sw_logic/Debug/src/helloworld.d
@@ -0,0 +1,54 @@
+src/helloworld.o src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters.h:
---
+++ Zynq_led/Zynq_led.sdk/sw_logic/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.d: ../src/platform.c \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../sw_logic_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../sw_logic_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
